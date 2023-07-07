e3018b9a8a2c01cf438ed465f4abcad3b8d00ff5
JKV21 <vtechgen21@gmail.com>
2023-07-07 10:42:25 +0200

    ...

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -25,13 +25,13 @@ const Ball = (props) => {
           polygonOffsetFactor={-5}
           flatShading
         />
-        <Decal
+        {/* <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
           map={decal}
           flatShading
-        />
+        /> */}
       </mesh>
     </Float>
   );
