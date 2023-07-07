f39d282e2862b5bd28d9cfda5c786a1b481be833
JKV21 <vtechgen21@gmail.com>
2023-07-07 11:12:02 +0200

    ...

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,6 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
+import * as THREE from 'three'
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
@@ -53,32 +54,27 @@ const ComputersCanvas = () => {
     };
   }, []);
 
-  try {
-    return (
-      <Canvas
-        frameloop="demand"
-        shadows
-        dpr={[1, 2]}
-        camera={{ position: [20, 3, 5], fov: 25 }}
-        gl={{ preserveDrawingBuffer: true }}
-      >
-        <Suspense fallback={<CanvasLoader />}>
-          <OrbitControls
-            enableZoom={false}
-            enablePan={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-          <Computers isMobile={isMobile} />
-        </Suspense>
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
 
-        <Preload all />
-      </Canvas>
-    );
-  } catch (error) {
-    alert(error);
-    return <h1>Error occurred while rendering...</h1>;
-  }
+      <Preload all />
+    </Canvas>
+  );
 };
 
 export default ComputersCanvas;
